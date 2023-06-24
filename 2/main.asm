7aaa06628bd17645866186601451217d26f167f9
Pasan Perera <pasansanjula.android@gmail.com>
2023-06-24 11:34:11 +0530

    more tabs headache

--- 2/main.asm
+++ 2/main.asm
@@ -28,7 +28,7 @@ StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
 ; CLK  -> 0x02
 ; DATA -> 0x04
 
-init:		bis.w #110011001b, R5			; data
+init:		        bis.w #110011001b, R5			; data
                 bis.w #100000000b, R6			; bit no.
                 bis.w #0x0a, R7				; i
 
@@ -49,13 +49,13 @@ begin:          bit.w R5, R6				; AND operation of the particular bit
 
 bitiszero:      dec.b R7
                 call #condition
-		bic.b #0x04, P1OUT			; DATA -> LOW
-		bis.b #0x02, P1OUT			; CLK -> HIGH
-		bic.b #0x02, P1OUT			; CLK -> LOW
-		ret
+		            bic.b #0x04, P1OUT			; DATA -> LOW
+		            bis.b #0x02, P1OUT			; CLK -> HIGH
+		            bic.b #0x02, P1OUT			; CLK -> LOW
+		            ret
 
-bitisone:	dec.b R7
-		call #condition
+bitisone:	      dec.b R7
+		            call #condition
                 bis.b #0x04, P1OUT			; DATA -> HIGH
                 bis.b #0x02, P1OUT			; CLK -> HIGH
                 bic.b #0x02, P1OUT			; CLK -> LOW
