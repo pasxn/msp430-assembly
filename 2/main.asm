25f82ddfc4e87b81d18aca04cd9d5491f9812123
Pasan Perera <pasansanjula.android@gmail.com>
2023-06-24 11:32:53 +0530

    eww tabs

--- 2/main.asm
+++ 2/main.asm
@@ -29,26 +29,26 @@ StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
 ; DATA -> 0x04
 
 init:		bis.w #110011001b, R5			; data
-		bis.w #100000000b, R6			; bit no.
-		bis.w #0x0a, R7				; i
+                bis.w #100000000b, R6			; bit no.
+                bis.w #0x0a, R7				; i
 
-		bis.b #0x07, P1DIR
-
-
-main:		bic.b #0x01, P1OUT
-            	bic.b #0x02, P1OUT
+                bis.b #0x07, P1DIR
 
 
-begin:		bit.w R5, R6				; AND operation of the particular bit
-		jz bitiszero
-		bit.w R5, R6
-		jnz bitisone
-		rlc R5
-		jmp begin
+main:           bic.b #0x01, P1OUT
+                bic.b #0x02, P1OUT
 
 
-bitiszero:	dec.b R7
-		call #condition
+begin:          bit.w R5, R6				; AND operation of the particular bit
+                jz bitiszero
+                bit.w R5, R6
+                jnz bitisone
+                rlc R5
+                jmp begin
+
+
+bitiszero:      dec.b R7
+                call #condition
 		bic.b #0x04, P1OUT			; DATA -> LOW
 		bis.b #0x02, P1OUT			; CLK -> HIGH
 		bic.b #0x02, P1OUT			; CLK -> LOW
@@ -56,18 +56,18 @@ bitiszero:	dec.b R7
 
 bitisone:	dec.b R7
 		call #condition
-		bis.b #0x04, P1OUT			; DATA -> HIGH
-		bis.b #0x02, P1OUT			; CLK -> HIGH
-		bic.b #0x02, P1OUT			; CLK -> LOW
-		ret
+                bis.b #0x04, P1OUT			; DATA -> HIGH
+                bis.b #0x02, P1OUT			; CLK -> HIGH
+                bic.b #0x02, P1OUT			; CLK -> LOW
+                ret
 
-condition:	cmp.w #0x00, R7
-		jz end
-		ret
+condition:      cmp.w #0x00, R7
+                jz end
+                ret
 
-end:		bis.b #0x01, P1OUT			; CS -> HIGH
-		bic.b #0x01, P1OUT			; CS -> LOW
-		jmp main
+end:            bis.b #0x01, P1OUT			; CS -> HIGH
+                bic.b #0x01, P1OUT			; CS -> LOW
+                jmp main
 		
 ;-------------------------------------------------------------------------------
 ; Stack Pointer definition
